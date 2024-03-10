2342c3abe95b8358d91af25653fbdf54695c36e9
Rodolfo Delduca <usodelduca@gmail.com>
2024-03-11 12:33:03 +1300

    add solution to 605

---
+++ 605-CanPlaceFlowers.js
@@ -0,0 +1,32 @@
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+*/
+
+// loop through array
+// find 3 zeroes
+// plant 1 in between them
+// decrease n
+// increase index
+// return when n is zero
+
+var canPlaceFlowers = function (flowerbed, n) {
+  let i = 0
+  while (i < flowerbed.length && n !== 0) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--
+      i++
+    }
+    i++
+  }
+  return n === 0
+}
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1))
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2))
